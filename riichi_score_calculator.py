379e34dff94aab608321e0b44ab2032bb6fe0791
image not found <finniganmadison@gmail.com>
2026-03-22 15:35:30 -0400

    Version 0.4.1
    Program once again can score multiple hands without restarting.

--- riichi_score_calculator.py
+++ riichi_score_calculator.py
@@ -1,6 +1,6 @@
 """
 Username: img404
-Version: 0.4.0
+Version: 0.4.1
 Purpose: Text-based python app for determining riichi mahjong score payments based on user input.
 """
 
@@ -8,6 +8,7 @@ Purpose: Text-based python app for determining riichi mahjong score payments bas
 import math
 
 
+# Defines valid sets and ranges for user input validation, as well as a constant later used for visual separation.
 yes_no = ["y", "n"]
 yakuman_types = ["counted yakuman", "single yakuman", "double yakuman"]
 valid_han_amounts = range(1,60)
@@ -16,6 +17,7 @@ valid_honba_amounts = range(0,20)
 SEPARATOR = "-" * 140
 
 
+# Function that introduces the program to the user.
 def introduction():
     print(SEPARATOR)
     print(SEPARATOR)
@@ -25,6 +27,7 @@ def introduction():
     print(SEPARATOR)
 
 
+# Function that requests and validates user input, storing them to new variables for use later.
 def hand_characteristics():
     while True:
         dealer_response = input("Were you the dealer? y/n: ").lower().strip()
@@ -39,12 +42,19 @@ def hand_characteristics():
             break
         print("Please enter either 'y' or 'n'.")
     while True:
+        honba = input("How many honba were on the table? Enter a numeral: ")
+        if int(honba) in valid_honba_amounts:
+            honba = int(honba)
+            break
+        print(f"Honba value cannot be '{honba}'. Please try again.")
+    while True:
         han = input("How many han did the hand score? Enter a numeral: ")
         if int(han) in valid_han_amounts:
             han = int(han)
             break
         else:
             print(f"Han value cannot be '{han}'. Please try again.")
+
     # If the han value is less than 5, then we must count fu. Otherwise, fu is given a placeholder value of 0.
     if han < 5:
         while True:
@@ -55,15 +65,10 @@ def hand_characteristics():
             print(f"Fu value cannot be '{fu}'. Please try again.")
     else:
         fu = 20
-    while True:
-        honba = input("How many honba were on the table? Enter a numeral: ")
-        if int(honba) in valid_honba_amounts:
-            honba = int(honba)
-            break
-        print(f"Honba value cannot be '{honba}'. Please try again.")
-    return dealer, tsumo, han, fu, honba
+    return dealer, tsumo, honba, han, fu
 
 
+# Function that uses han and fu to determine hand type and basic points, stored to new variables. Also includes yakuman validation.
 def type_and_points(han, fu):
     if han >= 13:
         while True:
@@ -99,24 +104,51 @@ def round_up_100(x):
     return math.ceil(x / 100) * 100
 
 
+# Function that uses previously defined variables to calculate the win type and payments, stored to new variables.
 def payment_calc(basic_points, dealer, tsumo, honba):
     if dealer:
         if tsumo:
+            win_type = "Dealer tsumo. "
             payment = str(round_up_100(2 * basic_points) + 100 * honba) + " from each player."
         else:
+            win_type = "Dealer ron. "
             payment = str(
                 round_up_100(6 * basic_points) + 300 * honba) + " from the player who discarded your winning tile."
     else:
         if tsumo:
+            win_type = "Non-dealer tsumo. "
             payment = str(round_up_100(basic_points) + 100 * honba) + " from each non-dealer player and " + str(
                 round_up_100(2 * basic_points) + 100 * honba) + " from the dealer."
         else:
+            win_type = "Non-dealer ron. "
             payment = str(
                 round_up_100(4 * basic_points) + 300 * honba) + " from the player who discarded your winning tile."
-    return payment
+    return win_type, payment
 
+
+# Function that prints hand information & payment details back to the user.
+def results(SEPARATOR, win_type, han, fu, honba, hand_type, payment):
+    print(SEPARATOR)
+    print(win_type)
+    if hand_type == 'normal':
+        print(f"{han} han, {fu} fu; {honba} honba.")
+    else:
+        print(f"{hand_type.capitalize()}, {honba} honba.")
+    print(f"Payment: {payment}")
+    print(SEPARATOR)
+
+
+# This is what runs when the user opens the program.
 introduction()
-dealer, tsumo, han, fu, honba = hand_characteristics()
-hand_type, basic_points = type_and_points(han,fu)
-payment = payment_calc(basic_points, dealer, tsumo, honba)
-print(f"Payment: {payment}")
+while True:
+    dealer, tsumo, honba, han, fu = hand_characteristics()
+    hand_type, basic_points = type_and_points(han,fu)
+    win_type, payment = payment_calc(basic_points, dealer, tsumo, honba)
+    results(SEPARATOR, win_type, han, fu, honba, hand_type, payment)
+    response = input("Score another hand? y/n: ")
+    if response not in yes_no:
+        print("Please enter either 'y' or 'n'.")
+        break
+    elif response == "n":
+        break
+    print(SEPARATOR)
